c381cd6f54992a53964c8960c234a6d5aa0523f8
Melly5 <polina.alina.2015@mail.ru>
2022-04-26 14:45:17 +0700

    add: Animal tree

---
+++ TvSet/AddObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvSet
+{
+    public class AddObject
+    {
+        public void Addobject()
+        {
+
+        }
+    }
+}
--- TvSet/MainWindow.xaml.cs
+++ TvSet/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace TvSet
 {
     /// <summary>
@@ -24,5 +25,83 @@ namespace TvSet
         {
             InitializeComponent();
         }
+        private void Click_t(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            ImageBrush treeImage = new ImageBrush();
+            int random = rand.Next(1,5);
+
+            switch (random)
+            {
+                case 1:
+                    treeImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Trees/tree1.png", UriKind.Relative));
+                    break;
+                case 2:
+                    treeImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Trees/tree2.png", UriKind.Relative));
+                    break;
+                case 3:
+                    treeImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Trees/tree3.png", UriKind.Relative));
+                    break;
+                case 4:
+                    treeImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Trees/tree4.png", UriKind.Relative));
+                    break;
+                case 5:
+                    treeImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Trees/tree5.png", UriKind.Relative));
+                    break;
+            }
+            Rectangle newTree = new Rectangle
+            {
+                Tag = "tree",
+                Height = rand.Next(70, 100),
+                Width = rand.Next(40, 50),
+                Fill = treeImage
+            };
+
+            Canvas.SetTop(newTree, rand.Next(40, 150));
+            Canvas.SetLeft(newTree, rand.Next(30, 300));
+
+            World.Children.Add(newTree);
+
+        }
+        private void Click_a(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            ImageBrush animalImage = new ImageBrush();
+            int random = rand.Next(1, 5);
+
+            switch (random)
+            {
+                case 1:
+                    animalImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Animals/animal1.png", UriKind.Relative));
+                    break;
+                case 2:
+                    animalImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Animals/animal2.png", UriKind.Relative));
+                    break;
+                case 3:
+                    animalImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Animals/animal3.png", UriKind.Relative));
+                    break;
+                case 4:
+                    animalImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Animals/animal4.png", UriKind.Relative));
+                    break;
+                case 5:
+                    animalImage.ImageSource = new BitmapImage(new Uri("C:/Users/Melly/source/repos/TvSet/TvSet/Images/Animals/animal5.png", UriKind.Relative));
+                    break;
+            }
+            Rectangle newAnimal = new Rectangle
+            {
+                Tag = "animal",
+                Height = rand.Next(70, 100),
+                Width = rand.Next(40, 50),
+                Fill = animalImage
+            };
+
+            Canvas.SetTop(newAnimal, rand.Next(40, 150));
+            Canvas.SetLeft(newAnimal, rand.Next(30, 300));
+
+            World.Children.Add(newAnimal);
+
+        }
+
+
     }
 }
